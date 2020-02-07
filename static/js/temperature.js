cd00d7a9385b4917c6c74ad344aa6335dd9ecfee
GildasPhilippe <gildas.philippe56@gmail.com>
2020-02-07 09:17:40 +0100

    Implemented temperature display on webapp

---
+++ static/js/temperature.js
@@ -0,0 +1,48 @@
+var card;
+var sensor;
+$(document).ready(function() {
+    $("#cardList").change(function(){
+        updateCard();});
+    $("#sensorList").change(function(){
+        updateSensor();});
+
+    updateCard();
+    updateSensor();
+    updateTemperature();
+});
+
+
+function updateCard(){
+    card = $("#cardList option:selected").text();
+    console.log(card + " selected");
+}
+function updateSensor(){
+    sensor = $("#sensorList option:selected").text();
+    console.log(sensor + " selected");
+}
+function updateTemperature(){
+    $.ajax({
+        url: '/get_temperature',
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify({"card": card, "sensor": sensor}),
+        processData: false,
+        success: function(result,status,xhr){
+            let temp = result["temp"];
+            if(parseInt(temp) <= 127){
+                displayTemperature("-");}
+            else{
+                displayTemperature(temp)}
+        },
+        error: function(xhr, errorThrown){
+            displayTemperature("-")
+        }
+    });
+}
+
+function displayTemperature(temp){
+    console.log("temp = " +temp)
+    $("#temp").text(temp);
+    setTimeout(updateTemperature, 3000)
+}
--- webapp.py
+++ webapp.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from jinja2 import Template
 import subprocess
 import json
+from pysnmp.hlapi import *
 
 
 app = Flask(__name__)
@@ -11,18 +12,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html.jinja2")
+    return render_template("home.html.jinja2", page="home")
 
 @app.route("/editor")
 def editor():
     with open("./arduino/examples/test_template.ino") as f:
         code = f.read()
-    return render_template("editor.html.jinja2", code=code)
+    return render_template("editor.html.jinja2", code=code, page="editor")
 
 
 @app.route("/temperature")
 def temperature():
-    return render_template("temperature.html.jinja2")
+    sensors = ["Sensor"+str(i) for i in range(1,17)]
+    ips = get_ips("ips.json")
+    cards = [card for card in ips]
+    return render_template("temperature.html.jinja2", sensors=sensors, cards=cards, page="temperature")
 
 
 # API
@@ -42,11 +46,45 @@ def upload():
         response[card_id] = 1 - update.returncode 
     return json.dumps(response)
 
+@app.route("/get_temperature", methods=["POST"])
+def get_temperature():
+    data = request.get_json()
+    card, sensor = data["card"], data["sensor"].strip("Sensor")
+    ips = get_ips('./ips.json')
+    ip = ips[card]
+    oid = '.1.3.6.1.4.1.5.'+sensor
+    temp = snmpget(ip, oid)
+    print(temp)
+    if(type(temp)!=int):
+        temp = -127
+    return json.dumps({"temp": temp})
+
+# Other Functions
 
 def get_ips(filename):
     with open(filename) as json_file:
         ips = json.load(json_file)
     return ips
+
+def snmpget(ip, oid):
+    '''snmpget -v 1 -c public ip oid'''
+    g = getCmd(SnmpEngine(),
+               CommunityData('public', mpModel=0),
+               UdpTransportTarget((ip, 161)),
+               ContextData(),
+               ObjectType(ObjectIdentity(oid)))
+    errorIndication, errorStatus, errorIndex, varBinds = next(g)
+    if errorIndication:
+        print(errorIndication)
+    else:
+        if errorStatus:
+            print('%s at %s' % (
+                errorStatus.prettyPrint(),
+                errorIndex and varBinds[int(errorIndex)-1] or '?'))
+        else:
+            for name, val in varBinds:
+                return val/100  # int->float
+
 
 if __name__ == "__main__":
     app.run()
